639e69817bb1ac7668262fa6b6d31cf61ead26ed
cherchali.amar <dempile@hotmail.com>
2017-11-06 07:13:11 +0100

    users rest

---
+++ src/main/java/com/airhacks/streaming/UserRestService.java
@@ -0,0 +1,80 @@
+package com.airhacks.streaming;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Path("/users")
+public class UserRestService {
+
+	@GET
+	public Response getUser() {
+
+		List<Integer> list = Arrays.asList(1,2,3,-3,-2,-1,0);
+
+			Collections.sort(list, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return (o1 > o2 ? 1 :(o1 == o2 ? 0 : -1));
+			}
+		});
+		final List<String> friends =
+				Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
+		friends.forEach((final String name) -> System.out.println(name));
+
+		return Response.status(200).entity(Stream.of(-3,-2,-1,0,1,2,3).max(Math::max).get().toString()).build();
+
+	}
+
+	@GET
+	@Path("/vip")
+	public Response getUserVIP() {
+
+		return Response.status(200).entity("getUserVIP is called").build();
+
+	}
+
+	@GET
+	@Path("{name}")
+	public Response getUserByName(@PathParam("name") String name) {
+
+		return Response.status(200)
+				.entity("getUserByName is called, name : " + name).build();
+
+	}
+
+	@GET
+	@Path("{id : \\d+}")
+	public Response getUserById(@PathParam("id") String id) {
+
+		return Response.status(200).entity("getUserById is called, id : " + id)
+				.build();
+
+	}
+
+	@GET
+	@Path("/username/{username : [a-zA-Z][a-zA-Z_0-9]}")
+	public Response getUserByUserName(@PathParam("username") String username) {
+
+		return Response.status(200)
+				.entity("getUserByUserName is called, username : " + username)
+				.build();
+
+	}
+
+	@GET
+	@Path("/books/{isbn : \\d+}")
+	public Response getUserBookByISBN(@PathParam("isbn") String isbn) {
+
+		return Response.status(200)
+				.entity("getUserBookByISBN is called, isbn : " + isbn).build();
+
+	}
+
+}
